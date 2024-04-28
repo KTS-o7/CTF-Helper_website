1a91daefc844712e537952718179ffa9b966410c
Krishnatejaswi S <shentharkrishnatejaswi@gmail.com>
2024-04-28 17:33:42 +0530

    Added backspace and capslock support

--- script.js
+++ script.js
@@ -23,13 +23,19 @@ $(function () {
         break;
       case "clear":
         terminal.text("");
+        printLine("Welcome to my hacker terminal!");
+        printLine(
+          'Type "flag" to get the flag, "hello" to say hello or "clear" to clear the terminal.'
+        );
         break;
       default:
         printLine("\nUnknown command: " + command);
     }
   }
 
-  $(document).keypress(function (e) {
+  var capsLock = false; // Add a variable to keep track of the Caps Lock state
+
+  $(document).keydown(function (e) {
     if (e.which === 13) {
       // Enter key
       var command = terminal
@@ -39,18 +45,35 @@ $(function () {
         .substring(inputLine.length - 1);
       handleCommand(command);
       printLine(inputLine);
+    } else if (e.which === 8) {
+      // Backspace key
+      var text = terminal.text();
+      terminal.text(text.slice(0, -1));
+    } else if (e.which >= 65 && e.which <= 90) {
+      // Letter key
+      var isUppercase =
+        (e.which >= 65 && e.which <= 90 && e.shiftKey) || capsLock;
+      var charCode = isUppercase ? e.which : e.which + 32;
+      terminal.append(String.fromCharCode(charCode));
+    } else if (e.which === 20) {
+      // Caps Lock key
+      capsLock = !capsLock;
     } else {
+      // Any other key
       terminal.append(String.fromCharCode(e.which));
     }
     return false;
   });
+
   $("#flagButton").click(function () {
     var flag = generateFlag();
     printLine("FLAG{" + flag + "}");
   });
 
   printLine("Welcome to my hacker terminal!");
-  printLine('Type "flag" to get the flag, or "hello" to say hello.');
+  printLine(
+    'Type "flag" to get the flag, "hello" to say hello or "clear" to clear the terminal.'
+  );
   printLine(inputLine);
   const actualFlag = "FLAG{Goes here}";
 });
